92a3756e7b7a20f12f040b6bd7fe91c749e6366a
Creepynax <116552630+Creepynax@users.noreply.github.com>
2022-11-17 14:05:23 +0100

    update delete.php

--- delete.php
+++ delete.php
@@ -3,4 +3,12 @@
 <?php
     session_start();
     include "includes/connect.php";
+
+    $response = $bdd -> query('SELECT * FROM Livre');
+    $donnees = $response -> fetch();
+
+    $livreID = $donnees['id'];
+    $supprLigne = $bdd -> prepare('DELETE FROM Livre WHERE id = $livreID');
+
+    header('Location : list.php');
 ?>
